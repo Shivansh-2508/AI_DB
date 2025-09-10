57a1fe6f121d8fc4bb266af36df46481ab6871db
Akshad <gawdeakshad@gmail.com>
2025-09-11 00:04:38 +0530

    oops cors

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,8 @@ from nlp_to_sql import (
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
+CORS(app, origins=["http://localhost:3000",
+     "https://ai-db-one.vercel.app"], supports_credentials=True)
 bcrypt = Bcrypt(app)
 
 app.config["JWT_SECRET_KEY"] = "super-secret-key"  # TODO: set via env var
